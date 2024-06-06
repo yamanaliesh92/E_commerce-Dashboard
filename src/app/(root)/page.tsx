46d120cfc6e595d22fc07157c16959ce6edef46a
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-06 20:02:32 +0300

    added shadcn-ui

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      hello
+      <Button>Hello</Button>
+    </>
+  );
+}
