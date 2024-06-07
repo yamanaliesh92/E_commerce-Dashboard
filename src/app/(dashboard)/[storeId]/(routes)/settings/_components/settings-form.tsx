3918ed627094062c479e6d1f4f675bf02f541196
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-07 20:36:00 +0300

    WIP:add settings-form file

---
+++ src/app/(dashboard)/[storeId]/(routes)/settings/_components/settings-form.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { Store } from "@prisma/client";
+import React from "react";
+
+interface SettingsFromProps {
+  initialData: Store;
+}
+
+export default function SettingsForm({ initialData }: SettingsFromProps) {
+  return (
+    <div className="flex items-center justify-between">
+      <Heading title="Settings" description="Manage store preferences" />
+    </div>
+  );
+}
--- src/app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -2,6 +2,7 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
+import SettingsForm from "./_components/settings-form";
 
 export default async function SettingsPage({
   params,
@@ -21,7 +22,9 @@ export default async function SettingsPage({
   }
   return (
     <div className="flex-col">
-      <div className="flex-1 space-y-4 p-8 pt-6"></div>
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SettingsForm initialData={store} />
+      </div>
     </div>
   );
 }
