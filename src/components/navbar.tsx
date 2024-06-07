2cb64b57869fc03c6b3afffb1ecc83b100c26c42
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-07 16:16:44 +0300

    WIP:add main-nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,15 @@
 import { UserButton } from "@clerk/nextjs";
 import React from "react";
+import MainNav from "./main-nav";
 
 export default function Navbar() {
   return (
     <div className="border-b">
       <div className="flex items-center px-4 h-16">
         <div>This will be a store switcher</div>
-        <div>This is will be the route</div>
+        <MainNav className="mx-4" />
         <div className="ml-auto flex items-center space-x-4">
-          <UserButton />
+          <UserButton afterSignOutUrl="/" />
         </div>
       </div>
     </div>
