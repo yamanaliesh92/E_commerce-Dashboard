8748847d9ed28327a2fa0d50491ea26aac3ab5d9
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-07 17:53:16 +0300

    fetch all stores in nvabar-file

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,22 @@
+import { db } from "@/lib/db";
 import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import React from "react";
 import MainNav from "./main-nav";
 import StoreSwitcher from "./store-switcher";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/sign-in");
+  }
+  const stores = await db.store.findMany({ where: { userId } });
+
   return (
     <div className="border-b">
       <div className="flex items-center px-4 h-16">
-        <StoreSwitcher />
+        <StoreSwitcher items={stores} />
         <MainNav className="mx-4" />
         <div className="ml-auto flex items-center space-x-4">
           <UserButton afterSignOutUrl="/" />
