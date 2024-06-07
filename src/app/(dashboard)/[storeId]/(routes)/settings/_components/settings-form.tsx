67bffbc93b31f59b213d15ebf5a53504cd4bb015
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-07 21:24:13 +0300

    WIP:added updated- form to settings-form file

--- src/app/(dashboard)/[storeId]/(routes)/settings/_components/settings-form.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/settings/_components/settings-form.tsx
@@ -5,7 +5,18 @@ import { Separator } from "@/components/ui/separator";
 import { Store } from "@prisma/client";
 import { Trash } from "lucide-react";
 import * as z from "zod";
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 interface SettingsFromProps {
   initialData: Store;
@@ -17,15 +28,58 @@ const formSchema = z.object({
 type SettingsFormValue = z.infer<typeof formSchema>;
 
 export default function SettingsForm({ initialData }: SettingsFromProps) {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const form = useForm<SettingsFormValue>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const onSubmit = async (data: SettingsFormValue) => {};
+
   return (
     <>
       <div className="flex items-center justify-between">
         <Heading title="Settings" description="Manage store preferences" />
-        <Button variant={"destructive"} size="sm">
+        <Button
+          disabled={loading}
+          onClick={() => setOpen(true)}
+          variant={"destructive"}
+          size="sm"
+        >
           <Trash className="h-4 w-4" />
         </Button>
       </div>
       <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full space-y-8"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Store name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} type="submit">
+            Save changes
+          </Button>
+        </form>
+      </Form>
     </>
   );
 }
