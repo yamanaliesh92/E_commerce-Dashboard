6424c6996f266440b5e1efdc7d18cc83cde99a35
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-07 21:08:23 +0300

    added button and separator to settings-form file

--- src/app/(dashboard)/[storeId]/(routes)/settings/_components/settings-form.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/settings/_components/settings-form.tsx
@@ -1,6 +1,9 @@
 "use client";
 import Heading from "@/components/heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
 import React from "react";
 
 interface SettingsFromProps {
@@ -9,8 +12,14 @@ interface SettingsFromProps {
 
 export default function SettingsForm({ initialData }: SettingsFromProps) {
   return (
-    <div className="flex items-center justify-between">
-      <Heading title="Settings" description="Manage store preferences" />
-    </div>
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="Manage store preferences" />
+        <Button variant={"destructive"} size="sm">
+          <Trash className="h-4 w-4" />
+        </Button>
+      </div>
+      <Separator />
+    </>
   );
 }
