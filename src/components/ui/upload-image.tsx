68cbc6717d8b9fb02d3f20aebc2250abb9c6828e
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-08 19:36:52 +0300

    WIP:added image-upload file

---
+++ src/components/ui/upload-image.tsx
@@ -0,0 +1,73 @@
+"use client";
+import { ImagePlus, Trash } from "lucide-react";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { Button } from "./button";
+import { CldUploadWidget } from "next-cloudinary";
+
+interface UploadImageProps {
+  value: string[];
+  disabled?: boolean;
+  onRemove: (value: string) => void;
+  onChange: (value: string) => void;
+}
+
+export default function ImageUpload({
+  value,
+  onChange,
+  onRemove,
+  disabled,
+}: UploadImageProps) {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => setIsMounted(true), []);
+
+  const onUpload = (result: any) => {
+    onChange(result.info.secure_url);
+  };
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <div>
+      <div className="mb-4 flex items-center gap-4">
+        {value.map((url) => (
+          <div
+            key={url}
+            className="w-[200px] relative h-[200px] rounded-md overflow-hidden"
+          >
+            <div className="z-10 absolute top-2 right-2">
+              <Button
+                type={"button"}
+                onClick={() => onRemove(url)}
+                variant={"destructive"}
+              >
+                <Trash className="h-4 w-4" />
+              </Button>
+            </div>
+            <Image alt="Image" className="object-cover" src={url} />
+          </div>
+        ))}
+      </div>
+      <CldUploadWidget uploadPreset="j0am2dyf" onUploadAdded={onUpload}>
+        {({ open }) => {
+          const onClick = () => {
+            open();
+          };
+          return (
+            <Button
+              disabled={disabled}
+              type="button"
+              variant={"secondary"}
+              onClick={onClick}
+            >
+              <ImagePlus className="w-4 h-4 mr-2" />
+            </Button>
+          );
+        }}
+      </CldUploadWidget>
+    </div>
+  );
+}
