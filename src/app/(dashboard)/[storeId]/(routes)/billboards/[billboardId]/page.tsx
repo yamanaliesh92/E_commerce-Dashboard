719ded20e468cce72838e94c7d4f0495b4cd1725
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-08 18:32:05 +0300

    WIP:added billboard-form file

--- src/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,5 +1,6 @@
 import { db } from "@/lib/db";
 import React from "react";
+import BillboardFrom from "./_components/billboard-form";
 
 export default async function BillboardPage({
   params,
@@ -11,5 +12,11 @@ export default async function BillboardPage({
     where: { id: billboardId },
   });
 
-  return <div>Existing billboard:{billboard?.label}</div>;
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardFrom initialData={billboard} />
+      </div>
+    </div>
+  );
 }
