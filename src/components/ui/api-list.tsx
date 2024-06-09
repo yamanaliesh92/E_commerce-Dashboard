46613056af35719df482169c2e5fdefd07501550
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-09 22:22:55 +0300

    WIP:added baseUrl to api-list file

--- src/components/ui/api-list.tsx
+++ src/components/ui/api-list.tsx
@@ -1,9 +1,16 @@
+"use client";
+import { useParams, useRouter } from "next/navigation";
 import React from "react";
+import { useOrigin } from "../../../hook/use-origin";
 
 interface ApiListProps {
   entityName: string;
   entityIdName: string;
 }
-export default function ApiList({}: ApiListProps) {
+export default function ApiList({ entityIdName, entityName }: ApiListProps) {
+  const origin = useOrigin();
+  const params = useParams();
+
+  const baseUrl = `${origin}/api/${params.storeId}`;
   return <div>Api</div>;
 }
