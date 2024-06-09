6a8ea429188e120a5bee26615010e26f9c936b46
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-10 00:32:47 +0300

    added navgation to cateories page

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -18,6 +18,12 @@ export default function MainNav({
     },
 
     {
+      href: `/${params.storeId}/categories`,
+      label: "Categories",
+      active: pathName === `/${params.storeId}/categories`,
+    },
+
+    {
       href: `/${params.storeId}/billboards`,
       label: "Billboards",
       active: pathName === `/${params.storeId}/billboards`,
