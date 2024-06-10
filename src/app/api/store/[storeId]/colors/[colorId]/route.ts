c6e707ff52faa87b70d2aa1ec0b196a74c47834e
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-10 23:06:17 +0300

    added CRUD to color-api

---
+++ src/app/api/store/[storeId]/colors/[colorId]/route.ts
@@ -0,0 +1,101 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { storeId: string; colorId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return NextResponse.json("Unauthorized", { status: 400 });
+    }
+
+    if (!params.colorId) {
+      return NextResponse.json("Color id is required", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return NextResponse.json("StoreId is required", { status: 400 });
+    }
+
+    const body = await req.json();
+
+    const { name, value } = body;
+
+    if (!name) {
+      return NextResponse.json("Name is required", { status: 400 });
+    }
+
+    if (!value) {
+      return NextResponse.json("Value is required", { status: 400 });
+    }
+
+    const storeByUserId = await db.store.findFirst({
+      where: { id: params.storeId, userId },
+    });
+
+    if (!storeByUserId) {
+      return NextResponse.json("Unauthorized", { status: 403 });
+    }
+
+    const editColor = await db.color.updateMany({
+      where: { id: params.colorId },
+      data: { value, name },
+    });
+    return NextResponse.json(editColor);
+  } catch (error) {
+    console.log("[Color_PATCH]", error);
+    return NextResponse.json("internal server error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { storeId: string; colorId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return NextResponse.json("Unauthorized", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return NextResponse.json("Store id is required", { status: 400 });
+    }
+
+    if (!params.colorId) {
+      return NextResponse.json("Color id is required", { status: 400 });
+    }
+
+    const deleteColor = await db.color.deleteMany({
+      where: { id: params.colorId },
+    });
+
+    return NextResponse.json(deleteColor);
+  } catch (error) {
+    console.log("[SIZE_DECOLOR]", error);
+    return NextResponse.json("internal server error", { status: 500 });
+  }
+}
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { colorId: string } }
+) {
+  try {
+    if (!params.colorId) {
+      return NextResponse.json("Color  id is required", { status: 400 });
+    }
+
+    const GetColor = await db.color.findUnique({
+      where: { id: params.colorId },
+    });
+
+    return NextResponse.json(GetColor);
+  } catch (error) {
+    console.log("[COLOR_DELETE]", error);
+    return NextResponse.json("internal server error", { status: 500 });
+  }
+}
---
+++ src/app/api/store/[storeId]/colors/route.ts
@@ -0,0 +1,72 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return NextResponse.json("Unauthenticated", { status: 400 });
+    }
+    const body = await req.json();
+
+    const { name, value } = body;
+
+    if (!params.storeId) {
+      return NextResponse.json("Store id is required", { status: 400 });
+    }
+
+    if (!name) {
+      return NextResponse.json("Name is required", { status: 400 });
+    }
+
+    if (!value) {
+      return NextResponse.json("Value is required", { status: 400 });
+    }
+
+    const storeByUserId = await db.store.findFirst({
+      where: { id: params.storeId, userId },
+    });
+
+    if (!storeByUserId) {
+      return NextResponse.json("Unauthorized", { status: 403 });
+    }
+
+    const color = await db.color.create({
+      data: { name, value, storeId: params.storeId },
+    });
+
+    return NextResponse.json(color);
+  } catch (error) {
+    console.log("[COLOR_POST]", error);
+    return NextResponse.json("internal server error", { status: 500 });
+  }
+}
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return NextResponse.json("Unauthenticated", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return NextResponse.json("Store id is required", { status: 400 });
+    }
+
+    const colors = await db.color.findFirst({
+      where: { storeId: params.storeId },
+    });
+
+    return NextResponse.json(colors);
+  } catch (error) {
+    console.log("[COLOR_GET]", error);
+    return NextResponse.json("internal server error", { status: 500 });
+  }
+}
