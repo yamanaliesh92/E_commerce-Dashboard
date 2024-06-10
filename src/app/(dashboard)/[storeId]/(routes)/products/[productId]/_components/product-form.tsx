ca3e9c3e79adfb49decfb73aecefd7e3fe0627fd
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-11 02:34:05 +0300

    added checkbox for isArchived  in product-form file

--- src/app/(dashboard)/[storeId]/(routes)/products/[productId]/_components/product-form.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/products/[productId]/_components/product-form.tsx
@@ -342,6 +342,27 @@ export default function ProductFrom({
                 </FormItem>
               )}
             />
+
+            <FormField
+              control={form.control}
+              name="isArchived"
+              render={({ field }) => (
+                <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+                  <FormControl>
+                    <Checkbox
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
+                  <div className="space-y-1 leading-none">
+                    <FormLabel>Archived</FormLabel>
+                    <FormDescription>
+                      this product will not appear anywhere in the store
+                    </FormDescription>
+                  </div>
+                </FormItem>
+              )}
+            />
           </div>
           <Button disabled={loading} type="submit">
             {action}
