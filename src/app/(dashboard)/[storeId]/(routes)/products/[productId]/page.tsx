4dc7f517b7719c0be5f50e677b8dd8b13c12d608
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-11 02:08:49 +0300

    WIP:get all categories and sizes and colors from database  in product-page

--- src/app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
@@ -5,18 +5,27 @@ import ProductFrom from "./_components/product-form";
 export default async function ProductPage({
   params,
 }: {
-  params: { productId: string };
+  params: { productId: string; storeId: string };
 }) {
-  const { productId } = params;
+  const { productId, storeId } = params;
   const product = await db.product.findUnique({
     where: { id: productId },
     include: { Images: true },
   });
 
+  const categories = await db.category.findMany({ where: { storeId } });
+  const sizes = await db.size.findMany({ where: { storeId } });
+  const colors = await db.color.findMany({ where: { storeId } });
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <ProductFrom initialData={product} />
+        <ProductFrom
+          sizes={sizes}
+          colors={colors}
+          categories={categories}
+          initialData={product}
+        />
       </div>
     </div>
   );
