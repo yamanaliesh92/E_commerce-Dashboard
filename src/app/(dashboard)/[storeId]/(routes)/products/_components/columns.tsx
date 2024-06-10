4dfd36c40a3f1c8f20aa9f87c84a1c4f4f49016f
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-11 01:06:19 +0300

    WIP:change the columns object  in columns-file

--- src/app/(dashboard)/[storeId]/(routes)/products/_components/columns.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/products/_components/columns.tsx
@@ -17,8 +17,43 @@ export type ProductColumn = {
 
 export const columns: ColumnDef<ProductColumn>[] = [
   {
-    accessorKey: "label",
-    header: "Label",
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "isArchived",
+    header: "Archived",
+  },
+  {
+    accessorKey: "isFeatured",
+    header: "Featured",
+  },
+
+  {
+    accessorKey: "price",
+    header: "Price",
+  },
+
+  {
+    accessorKey: "category",
+    header: "Category",
+  },
+  {
+    accessorKey: "size",
+    header: "Size",
+  },
+  {
+    accessorKey: "color",
+    header: "Color",
+    cell: ({ row }) => (
+      <div className="flex items-center gap-x-2">
+        {row.original.color}
+        <div
+          className="h-6 w-6 rounded-full border"
+          style={{ backgroundColor: row.original.color }}
+        />
+      </div>
+    ),
   },
   {
     accessorKey: "created",
