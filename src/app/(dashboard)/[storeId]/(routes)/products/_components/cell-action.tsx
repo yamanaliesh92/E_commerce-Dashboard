324b4628dcb290f93b89b5214ab1d3b2349080ca
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-11 16:22:11 +0300

    added some updates for update method  in api page

--- src/app/(dashboard)/[storeId]/(routes)/products/_components/cell-action.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/products/_components/cell-action.tsx
@@ -27,7 +27,7 @@ export const CellAction = ({ data }: CellActionProps) => {
   const params = useParams();
   const onCopy = (id: string) => {
     navigator.clipboard.writeText(id);
-    toast.success("Billboard id copied to the clipboard");
+    toast.success("Product id copied to the clipboard");
   };
 
   const onDelete = async () => {
--- src/app/api/store/[storeId]/products/[productId]/route.ts
+++ src/app/api/store/[storeId]/products/[productId]/route.ts
@@ -22,7 +22,44 @@ export async function PATCH(
 
     const body = await req.json();
 
-    const { value, images } = body;
+    const {
+      name,
+      sizeId,
+      colorId,
+      Images,
+      isFeatured,
+      isArchived,
+      categoryId,
+      price,
+    } = body;
+
+    if (!params.storeId) {
+      return NextResponse.json("Store id is required", { status: 400 });
+    }
+
+    if (!Images || !Images.length) {
+      return NextResponse.json("Images is required", { status: 400 });
+    }
+
+    if (!name) {
+      return NextResponse.json("Name is required", { status: 400 });
+    }
+
+    if (!sizeId) {
+      return NextResponse.json("Size id is required", { status: 400 });
+    }
+
+    if (!price) {
+      return NextResponse.json("Price id is required", { status: 400 });
+    }
+
+    if (!categoryId) {
+      return NextResponse.json("Category id is required", { status: 400 });
+    }
+
+    if (!colorId) {
+      return NextResponse.json("Color id is required", { status: 400 });
+    }
 
     const storeByUserId = await db.store.findFirst({
       where: { id: params.storeId, userId },
@@ -34,14 +71,23 @@ export async function PATCH(
 
     await db.product.update({
       where: { id: params.productId },
-      data: { ...value, Images: { deleteMany: {} } },
+      data: {
+        name,
+        colorId,
+        sizeId,
+        categoryId,
+        price,
+        isArchived,
+        isFeatured,
+        Images: { deleteMany: {} },
+      },
     });
     const product = await db.product.update({
       where: { id: params.productId },
       data: {
         Images: {
           createMany: {
-            data: [...images.map((image: { url: string }) => image)],
+            data: [...Images.map((image: { url: string }) => image)],
           },
         },
       },
