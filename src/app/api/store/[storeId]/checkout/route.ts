78b55e3a1e964d10b6246bdca4d6fc6399830985
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-15 15:27:11 +0300

    added webhook to api page

--- src/app/api/store/[storeId]/checkout/route.ts
+++ src/app/api/store/[storeId]/checkout/route.ts
@@ -5,8 +5,8 @@ import Stripe from "stripe";
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods ": "GET,POST,DELETE,OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type,Authorization",
+  "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+  "Access-Control-Allow-Headers": "X-Requested-With,Content-Type,Authorization",
 };
 
 export async function OPTIONS() {
@@ -17,56 +17,64 @@ export async function POST(
   req: NextRequest,
   { params }: { params: { storeId: string } }
 ) {
-  const productIds = await req.json();
-  if (!productIds || productIds.length === 0) {
-    return NextResponse.json("Product ids are required", { status: 400 });
-  }
-  const products = await db.product.findMany({
-    where: { id: { in: productIds } },
-  });
+  try {
+    const { productIds } = await req.json();
+    if (!productIds || productIds.length === 0) {
+      return NextResponse.json("Product ids are required", { status: 400 });
+    }
+    console.log("Ids", productIds);
+    const products = await db.product.findMany({
+      where: { id: { in: productIds } },
+    });
+    console.log("product", products);
 
-  const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
-  products.forEach((product) => {
-    line_items.push({
-      quantity: 1,
-      price_data: {
-        currency: "USD",
-        product_data: {
-          name: product.name,
+    const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+
+    products.forEach((product) => {
+      line_items.push({
+        quantity: 1,
+        price_data: {
+          currency: "USD",
+          product_data: {
+            name: product.name,
+          },
+          unit_amount: product.price.toNumber() * 100,
         },
-        unit_amount: product.price.toNumber() * 100,
+      });
+    });
+
+    const order = await db.order.create({
+      data: {
+        storeId: params.storeId,
+        isPaid: false,
+        orderItem: {
+          create: productIds.map((productId: string) => ({
+            product: {
+              connect: {
+                id: productId,
+              },
+            },
+          })),
+        },
       },
     });
-  });
 
-  const order = await db.order.create({
-    data: {
-      storeId: params.storeId,
-      isPaid: false,
-      orderItem: {
-        create: productIds.map((productId: string) => ({
-          product: {
-            connect: {
-              id: productId,
-            },
-          },
-        })),
+    const session = await stripe.checkout.sessions.create({
+      line_items,
+      mode: "payment",
+      billing_address_collection: "required",
+      phone_number_collection: {
+        enabled: true,
       },
-    },
-  });
+      success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
+      cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
+      metadata: {
+        orderId: order.id,
+      },
+    });
 
-  const session = await stripe.checkout.sessions.create({
-    line_items,
-    mode: "payment",
-    billing_address_collection: "required",
-    phone_number_collection: {
-      enabled: true,
-    },
-    success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
-    cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
-    metadata: {
-      orderId: order.id,
-    },
-  });
-  return NextResponse.json({ url: session.url }, { headers: corsHeaders });
+    return NextResponse.json({ url: session.url }, { headers: corsHeaders });
+  } catch (error) {
+    console.log("error", error);
+  }
 }
---
+++ src/app/api/webhook/route.ts
@@ -0,0 +1,60 @@
+import { stripe } from "@/lib/stripe";
+import { PrismaClient } from "@prisma/client";
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import { Stripe } from "stripe";
+
+const db = new PrismaClient();
+
+export async function POST(req: NextRequest) {
+  const body = await req.text();
+  const signature = headers().get("Stripe-Signature") as string;
+
+  let event: Stripe.Event;
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+  } catch (error: any) {
+    return NextResponse.json(`Webhook Error ${error.message}`, { status: 500 });
+  }
+
+  const session = event.data.object as Stripe.Checkout.Session;
+  const address = session.customer_details?.address;
+
+  const addressComponents = [
+    address?.line1,
+    address?.line2,
+    address?.city,
+    address?.state,
+    address?.country,
+    address?.postal_code,
+  ];
+
+  const addressString = addressComponents.filter((c) => c !== null).join(`, `);
+
+  if (event.type === "checkout.session.completed") {
+    const order = await db.order.update({
+      where: { id: session?.metadata?.orderID },
+      data: {
+        isPaid: true,
+        address: addressString,
+        phone: session?.customer_details?.phone || "",
+      },
+      include: { orderItem: true },
+    });
+    const productIds = order.orderItem.map((orderItem) => orderItem.productId);
+    await db.product.updateMany({
+      where: { id: { in: [...productIds] } },
+      data: { isArchived: true },
+    });
+  } else {
+    return NextResponse.json(
+      `Webhook Error:Unhandled event type ${event.type}`,
+      { status: 200 }
+    );
+  }
+  return NextResponse.json(null, { status: 200 });
+}
