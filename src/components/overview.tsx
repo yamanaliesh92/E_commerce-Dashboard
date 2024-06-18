4c01a8176a03f0a2642788adf960efefe32e31ff
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-18 17:56:35 +0300

    added overview  file

---
+++ src/components/overview.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { Card } from "@/components/ui/card";
+import React from "react";
+import { Bar, YAxis, XAxis, ResponsiveContainer, BarChart } from "recharts";
+
+interface OverviewProps {
+  data: any[];
+}
+
+export default function Overview({ data }: OverviewProps) {
+  return (
+    <ResponsiveContainer width="100%" height={350}>
+      <BarChart data={data}>
+        <XAxis
+          dataKey="name"
+          stroke="#888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+        />
+        <YAxis
+          stroke="#888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+          tickFormatter={(value) => `$${value}`}
+        />
+        <Bar dataKey="total" fill="#0369a1" radius={[4, 4, 0, 0]} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+}
