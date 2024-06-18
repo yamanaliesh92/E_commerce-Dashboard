44c882b965a3030f9fa95bb3664270fbedecf736
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-18 17:52:09 +0300

    added get-stock-count file

---
+++ action/get-stock-count.ts
@@ -0,0 +1,9 @@
+import { db } from "@/lib/db";
+
+export const getStockCount = async (storeId: string) => {
+  const stockCount = await db.product.count({
+    where: { storeId, isArchived: false },
+  });
+
+  return stockCount;
+};
--- src/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -5,6 +5,7 @@ import formatPrice from "@/lib/utils";
 import { CreditCard, DollarSign, Package } from "lucide-react";
 import React from "react";
 import { getSalesCount } from "../../../../../action/get-sales-count";
+import { getStockCount } from "../../../../../action/get-stock-count";
 import { getTotalRevenue } from "../../../../../action/get-total-revenue";
 
 export default async function DashboardPage({
@@ -14,6 +15,7 @@ export default async function DashboardPage({
 }) {
   const totalRevenue = await getTotalRevenue(params.storeId);
   const salesCount = await getSalesCount(params.storeId);
+  const stockCount = await getStockCount(params.storeId);
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6 ">
@@ -40,7 +42,7 @@ export default async function DashboardPage({
               <CreditCard className="h-4 w-4 text-muted-foreground" />
             </CardHeader>
             <CardContent>
-              <div className="text-2xl font-bold">+25</div>
+              <div className="text-2xl font-bold">{salesCount}</div>
             </CardContent>
           </Card>
 
@@ -52,7 +54,7 @@ export default async function DashboardPage({
               <Package className="h-4 w-4 text-muted-foreground" />
             </CardHeader>
             <CardContent>
-              <div className="text-2xl font-bold">12</div>
+              <div className="text-2xl font-bold">{stockCount</div>
             </CardContent>
           </Card>
         </div>
